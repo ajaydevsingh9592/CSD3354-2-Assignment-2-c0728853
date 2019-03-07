f7c7298277667e725ab2d7183c90e06262a8ae85
visualstudio <visualstudio@230-PC39>
2019-03-07 14:16:50 -0500

    Lab 01 Solved

--- CSD3354-2-Assignment-2-c0728853/Program.cs
+++ CSD3354-2-Assignment-2-c0728853/Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CSD3354_2_Assignment_2_c0728853
+//AJAY DEV SINGH C0728853
+//SANDEEP BEHAL C0727520
+//CSD3354 SECTION 1
+//ASSIGNMENT 2
+//March 7,2019
 {
     class Program
     {
         static void Main(string[] args)
         {
+        }
+    }
+    public class DelegateExcercises
+    {
+        public delegate void MyDelegate();
+
+        void Method1()
+        {
+            Console.WriteLine("Method1");
+            Console.ReadLine();
+        }
+        public void Method2()
+        {
+            MyDelegate mydelegate = new MyDelegate(Method1);
+            mydelegate();
         }
     }
 }
